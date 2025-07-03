fa1ea89d0611823e0a0b583622b2883d984ad8f4
gbduarteoliveira <gabrieloliveiraduarte14@gmail.com>
2025-07-02 21:33:31 -0300

    Add files via upload

---
+++ GB_ATT.py
@@ -0,0 +1,95 @@
+# Questão 1
+
+# def metros_p_centimetros(metros):
+#     centimetros = metros*100
+#     return centimetros
+
+
+# metros = float(input("Digite um valor em metros: "))
+# centimetros = metros_p_centimetros(metros)
+# print(f' {metros} metros em centimetros é igual a {centimetros} centimetros')
+
+# Questão 2
+
+# raio = float(input("Digite o valor do raio: "))
+# area = 3.14 * raio**2
+# print(f" A área de um circulo de raio {raio} é igual á {area}: ")
+
+
+# Questão 3
+# base = float(input("Digite o valor da base do quadrado : "))
+# altura = float(input("Digite o valor da altura do quadrado : "))
+
+# area = (base*altura)
+# dobro_area = area*2
+
+
+# print(f" A área de seu quadrado é igual a {area}, e o dobro dessa área é {dobro_area}")
+
+# Questão 4
+
+# Valor_hora = float(input("Quanto você ganha por hora ? R$"))
+# Horas_trabalhadas = float(input("Quantas horas trabalhadas no mês?"))
+
+# Salario = float(Horas_trabalhadas*Valor_hora)
+
+# print(f"Seu sálario é  R$ {Salario}")
+
+# Questão 5
+
+# Gigabytes = int(input("Digite o tamanho do seu arquivo em  Gigabytes : "))
+
+# Megabytes = Gigabytes * 1024
+# Kilobytes = Gigabytes * 1024 * 1024
+
+# print(f" Seu arquivo tem {Gigabytes} Gigabytes, já convertendo em Megabytes temos : {Megabytes} e epra Kilobytes teremos : {Kilobytes}.")
+
+# Questão 6
+
+
+# i = 1
+# lista_num = []
+
+# while i <= 4:
+#     num = int(input(f"Digite o {i}º numero: "))
+#     lista_num.append(num)
+#     print(num)
+
+#     i += 1
+
+# maior = max(lista_num)
+# print(f"O maior numero dessa lista é {maior}")
+
+# Questão 7
+
+# produto_1 = float(input("Digite o valor do produto 1 : R$"))
+# produto_2 = float(input("Digite o valor do produto 2 : R$ "))
+# produto_3 = float(input("Digite o valor do produto 3 : R$ "))
+
+# menor_preço = (min(produto_1, produto_2, produto_3))
+
+# if menor_preço == produto_1:
+#     produto_escolhido = produto_1
+
+# elif menor_preço == produto_2:
+#     produto_escolhido = produto_2
+# else:
+#     produto_escolhido = produto_3
+
+
+# print(f"O produto escolhido é R$ {menor_preço:.2f} ")
+
+# Questão 8
+
+# Turno = (input("Em que turno você estuda M(Matutino), V(Vespertino) e N(Noturno) ?")).upper()
+# if Turno == "M":
+#     print("Bom dia!")
+# elif Turno == "V":
+#     print("Boa tarde!")
+# elif Turno == "N":
+#     print("Boa noite!")
+# else:
+#     print("Valor inválido!")
+
+
+# Questão 9
